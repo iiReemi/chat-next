a0ca30ccde4054b4f73ee2847aa61be243de6b06
Ed Lima Projetos <edlimaprojetos@Eds-MacBook-Air.local>
2024-06-30 16:38:34 -0300

    add backend endpoint to .env files

--- src/socket.ts
+++ src/socket.ts
@@ -2,6 +2,8 @@
 
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000"); // substitua pela URL do seu servidor
+const socketUrl =
+  process.env.NEXT_PUBLIC_SOCKET_IO_URL || "http://localhost:4000"; // URL padrão caso a variável de ambiente não esteja definida
+const socket = io(socketUrl);
 
 export default socket;
