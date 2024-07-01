a30c74d0bb281bb84fb8fa8a033434dc88d00021
Ed Lima Projetos <edlimaprojetos@Eds-MacBook-Air.local>
2024-07-01 01:03:33 -0300

    add loading audio indicator

---
+++ src/components/LoadingAnimation/LoadingAnimation.tsx
@@ -0,0 +1,9 @@
+import "./styles.css"; // Ajuste o caminho conforme necessário
+
+export default function LoadingAnimation() {
+  return (
+    <div className="loading-animation w-full">
+      <div className="circle"></div>
+    </div>
+  );
+}
